c6d6def4835089e49d1b3ab6864fbf540508d041
Rafael <130803096+rafael019wi@users.noreply.github.com>
2026-04-19 14:43:10 -0300

    a

--- src/cap03/ex3.14.c
+++ src/cap03/ex3.14.c
@@ -10,7 +10,29 @@
 #include <stdlib.h>
 
 int main( void ) {
+    int n1;
+    int n2;
+    int somatorio = 0;
 
+    
+
+    printf("N1: ");
+    scanf("%d", &n1);
+
+    printf("\nN2: ");
+    scanf("%d", &n2);
+
+    if (n1> n2){
+        int temp = n1;
+        n1 = n2;
+        n2 = temp;
+    }
+
+    for (int i = n1; i<=n2; i++){
+        somatorio += i;
+    }
+
+    printf("\nSomatorio entre %d e %d: %d", n1,n2,somatorio);
     return 0;
 
 }
--- src/cap03/ex3.15.c
+++ src/cap03/ex3.15.c
@@ -10,7 +10,22 @@
 #include <stdlib.h>
 
 int main( void ) {
+    int numero;
+    int fatorial = 1;
+    
 
+    printf("Numero: ");
+    scanf("%d", &numero);
+    if (numero<= 0 ){
+        printf("Nao ha fatorial de numero negativo.");
+    }
+    else {
+        for (int i = numero; i>0;i--){
+        fatorial = fatorial*i;
+        
+        }
+        printf("%d! = %d",numero,fatorial);
+    }
     return 0;
 
 }
