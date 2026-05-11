8823d76c38bc8c67f36bbe5bb58c5c14f8d40d23
Rafael <130803096+rafael019wi@users.noreply.github.com>
2026-05-11 14:58:47 -0300

    A

--- src/cap05/ex5.2.c
+++ src/cap05/ex5.2.c
@@ -10,6 +10,35 @@
 #include <stdlib.h>
 
 int main( void ) {
+    int array1[3][3];
+    int array2[3][3];
+    int arraySoma[3][3];
+    
+    int linhas1 = (int) (sizeof(array1)/sizeof(array1[0]));
+    int colunas1 = (int) (sizeof(array1[0])/sizeof(array1[0][0]));
+
+    int linhas2 = (int) (sizeof(array2)/sizeof(array2[0]));
+    int colunas2 = (int) (sizeof(array2[0])/ sizeof(array2[0][0]));
+
+
+    for(int i = 0 ; i < linhas1; i++){
+        for (int j = 0 ; j<colunas1; j++){
+            printf("array1[%d][%d]: ",i,j);
+            scanf("%d", array1[i][j]);
+        }
+    }
+
+    for(int i = 0 ; i < linhas2; i++){
+        for (int j = 0 ; j<colunas2; j++){
+            printf("array2[%d][%d]: ",i,j);
+            scanf("%d", array2[i][j]);
+            arraySoma[i][j] = array1[i][j] + array2[i][j];
+        }
+        printf("\n");
+    }
+
+    
+
 
     return 0;
 
