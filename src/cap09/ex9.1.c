987f590f3e99174a68aecbf26778946500bf3bac
Rafael <130803096+rafael019wi@users.noreply.github.com>
2026-06-01 12:29:22 -0300

    \a

--- src/cap09/ex9.1.c
+++ src/cap09/ex9.1.c
@@ -8,9 +8,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main( void ) {
-
+    char string[41];
+    printf("String: ");
+    fgets( string, 41, stdin );
+    string[strlen(string)-1] = '\0';
+    for(int i = 0 ; i<4 ; i++){
+        printf("%c",string[i]);
+        if(i<3){
+            printf(", ");
+        }
+        else{
+            printf(".");
+        }
+        
+    }
     return 0;
 
 }
