32f17e22d6ed0b51c557b09d7de4c2655d675872
rafael019wi <130803096+rafael019wi@users.noreply.github.com>
2026-06-24 10:55:02 -0300

    a

--- src/cap09/ex9.7.c
+++ src/cap09/ex9.7.c
@@ -9,13 +9,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int main( void ) {
-    char string[5][40];
-    for(int i = 0 ; i<5 ; i++)
-    printf("Nome: ");
-    fgets(string, 41, stdin);
-    printf("Nome: ");
-    fgets(string, 41, stdin);
-    return 0;
 
+int main() {
+    char string1[5][40];
+    char string2[5][40];
+    for (int i = 0; i < 5; i++) {
+        printf("Par %d, palavra 1: ", i + 1);
+        fgets(string1[i], 40, stdin);
+        string1[i][strlen(string1[i])-1] = '\0';
+
+        printf("Par %d, palavra 2: ", i + 1);
+        fgets(string2[i], 40, stdin);
+        string2[i][strlen(string2[i])-1] = '\0';
+    }
+
+    for (int i = 0; i < 5; i++) {
+        int comp = strcmp(string1[i], string2[i]);
+        printf("%s - %s: ", string1[i], string2[i]);
+        if (comp == 0)
+            printf("IGUAIS\n");
+        else if (comp < 0)
+            printf("ORDEM CRESCENTE\n");
+        else
+            printf("ORDEM DECRESCENTE\n");
+    }
+    return 0;
 }
--- src/cap09/ex9.8.c
+++ src/cap09/ex9.8.c
@@ -8,8 +8,28 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main( void ) {
+    char string1[3][40];
+    char temp[40];
+    for (int i = 0; i < 3; i++) {
+        printf("String %d: ", i + 1);
+        fgets(string1[i], 40, stdin);
+        string1[i][strlen(string1[i])-1] = '\0';
+    }
+
+
+     for (int i = 0; i < 2; i++) {
+        for (int j = i + 1; j < 3; j++) {
+            if (strcmp(string1[i], string1[j]) > 0) {
+                strcpy(temp, string1[i]);
+                strcpy(string1[i], string1[j]);
+                strcpy(string1[j], temp);
+            }
+        }
+    }
+    printf("%s, %s e %s", string1[0], string1[1], string1[2]);
 
     return 0;
 
